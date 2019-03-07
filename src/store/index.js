18f458640d618ea6e6b07e41020c20c2ef6054b6
zhouming <minerchow@126.com>
2019-03-07 20:37:57 +0800

    devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import  thunk  from 'redux-thunk'
 import rootReducer from './reducers'
-
+const devTools = ISDEV ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
 let store = createStore(
     rootReducer,
     compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    devTools
 ));
 
 //window.store = store;
